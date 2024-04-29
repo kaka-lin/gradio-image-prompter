650bf2df5e28f77e8aecce8485b338c8feb2f7e6
Kaka <vn503024@gmail.com>
2024-04-29 17:57:54 +0800

    feat: update demo/app.py

--- demo/app.py
+++ demo/app.py
@@ -1,9 +1,23 @@
 import gradio as gr
 from gradio_image_prompter import ImagePrompter
 
+
+# Component is used as an input
+# demo = gr.Interface(
+#     fn=lambda prompts: (prompts["image"], prompts["points"]),
+#     inputs=ImagePrompter(show_label=False),
+#     outputs=[gr.Image(show_label=False), gr.Dataframe(label="Points")],
+# )
+
+# Component is used as an output
+# fn's fisrt argument can be: {"image": prompts["image"]}
+# or {"image": prompts["image"], "points": prompts["points"]}
 demo = gr.Interface(
-    lambda prompts: (prompts["image"], prompts["points"]),
-    ImagePrompter(show_label=False),
-    [gr.Image(show_label=False), gr.Dataframe(label="Points")],
+    fn=lambda prompts: (prompts["image"], prompts["points"]),
+    inputs=ImagePrompter(show_label=False),
+    outputs=[ImagePrompter(show_label=False), gr.Dataframe(label="Points")],
 )
-demo.launch()
+
+
+if __name__ == "__main__":
+    demo.launch()
